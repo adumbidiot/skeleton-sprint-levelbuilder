63b236a1cf55d3bf6063a4a5a3652ba6b5618174
nathaniel <nathaniel.daniel23@outlook.com>
2017-10-02 19:30:51 -0700

    Greenworks compiled

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 const electron = require('electron');
 const path = require('path');
 const url = require('url');
+const greenworks = require('greenworks');
 
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
